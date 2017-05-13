0e4a308b50a26cac2e33a7ea2ec64ba0a39c1121
Vaibhav <vaibhavsethi84@gmail.com>
2017-05-13 17:10:26 +1000

    use StringBuffer for string manipulation.

--- OrderLogging/src/com/utils/LoggerUtils.java
+++ OrderLogging/src/com/utils/LoggerUtils.java
@@ -2,15 +2,17 @@ package com.utils;
 
 public class LoggerUtils {
 	public static String truncate(String ipString,int maxLen){
-		String opString=ipString;
+		StringBuffer opString= new StringBuffer();
 		if(ipString.length() - maxLen >=2 && maxLen>22){
 			int charToPick = maxLen - 21;
 			int from = charToPick/2;
 			from = charToPick%2 > 0 ? from+1 : from;
 			int to = ipString.length() - (charToPick/2);
-			opString = ipString.substring(0, from) + " ... (truncated) ... " + ipString.substring(to);
+			opString = opString.append(ipString.substring(0, from)).append(" ... (truncated) ... ").append(ipString.substring(to));
+		}else{
+			return ipString;
 		}
-		return opString;
+		return opString.toString();
 	}
 	public static void log(String logString){
 		System.out.println(logString);
